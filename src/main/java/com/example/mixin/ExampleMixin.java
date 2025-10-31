4d02b893c88acb6e37d675f91b41582da2e458d5
Crystal0404 <syh040325@163.com>
2025-10-31 21:35:40 +0800

    mojmap

--- src/main/java/com/example/mixin/ExampleMixin.java
+++ src/main/java/com/example/mixin/ExampleMixin.java
@@ -9,9 +9,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftServer.class)
 public abstract class ExampleMixin {
-    @Inject(method = "loadWorld", at = @At("HEAD"))
+    @Inject(method = "loadLevel", at = @At("HEAD"))
     private void init(CallbackInfo ci) {
-        // This code is injected into the start of MinecraftServer.loadWorld()V
+        // This code is injected into the start of MinecraftServer.loadLevel()V
         TemplateMod.LOGGER.info("Hello World!");
     }
 }
